aad5a6d637cf76a79bf7d778a0dbc81a191f053d
jeanne24 <anne.gokotano@gmail.com>
2018-08-02 17:07:38 +0800

    initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,111 @@
-document.getElementById('btn-hide').onclick = () => {
-	document.getElementById('hide-show').style.display = 'none';
-}
-
-document.getElementById('btn-show').onclick = () => {
-	document.getElementById('hide-show').style.display = 'block';
-}
-
-document.getElementById('change-content').onclick = () => {
-	var txt = document.getElementById('txt-placeholder');
-	txt.innerHTML = "Buenas Dias!";
-	txt.style.color = 'mint';
-}
-
-document.getElementById('change-img-height').onclick = () => {
-	document.getElementById('img').height = '500';
-}
-document.getElementById('change-img').onclick = () => {
-	document.getElementById('img').src = 'https://media.giphy.com/media/6VoDJzfRjJNbG/giphy.gif';
-}
-document.getElementById('change-style').onclick = () => {
-	document.getElementById('change-my-style').style.fontFamily = 'Kirang Haerang';
-	document.getElementById('change-my-style').style.fontSize = '3em';
-	document.getElementById('change-my-style').style.color = 'red';
-}
-document.getElementById('add').onclick = () => {
-	var num1= document.getElementById('num1').value.trim();
-	var num2 = document.getElementById('num2').value.trim();
-	document.getElementById('num1').value="";
-	document.getElementById('num2').value="";
-	document.getElementById('result').innerHTML =parseInt(num1)+parseInt(num2);
-
-}
+let level = {
+	'1':'5',
+	'2':'7',
+	'3':'8',
+	'4':'9',
+	'5':'10'
+};
+let cards = [];
+let correctGuess = [];
+let score = $('#score');
+let currentScore =0;
+let card1,card2;
+let game = $('.game');
+let lostEnd = false;
 
 
 
+const makeGame = (pairs) => {
+	game.html("");
+	let numCards = pairs;
+    let flag = false;
+	for(let i = 1; i<=numCards; i++){
+       cards.push(i,i);
+    }//end for
+    console.log(cards);
+    shuffleArray(cards);
+    displayGame();
+}
+
+
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    console.log(array);
+}
+
+const displayGame = () => {
+	for(let i=0 ; i<cards.length ; i++){
+		$('.game').append('<div class="square col-2"><img class="card card'+i+'" value="'+cards[i]+'"></img></div>');
+		$('.card'+i).attr({
+			'src': 'assets/images/back.png'
+		});	
+	}
+}
+
+makeGame(5);
+
+$(".card").click( function() {
+	// console.log('hey');
+	if(!isAlreadyGuessed($(this).attr("value"))){
+		checkIfMatch();
+
+	$(this).attr({
+		'src': 'assets/images/'+ $(this).attr("value")+'.png'
+	});	
+	if(card1==null){
+		card1 = $(this);
+	}else{
+		card2 = $(this);
+	}
+
+	if(currentScore==4){
+		checkIfMatch();
+		checkIfWinner();
+	}
+	}
+	// if(/*check here if card 1 and card 2 match, if true score 1 and dont close the card, else close the cards*/)
+
+});
+
+const checkIfMatch = () => {
+
+	console.log(card1+' and '+ card2);
+	if(card2!=null && card2 != null){
+		console.log('card1 = ' + card1.attr("value") + ' and ' + card2.attr("value"));
+		if(card1.attr("value")==card2.attr("value")){
+			score.html(currentScore+=1);
+			correctGuess.push(card1.attr("value"));
+		}else{
+			card1.attr({
+				'src': 'assets/images/back.png'
+			});
+			card2.attr({
+				'src': 'assets/images/back.png'
+			});
+		}
+
+	card1=null;
+	card2=null;
+	}
+}
+
+const isAlreadyGuessed = (guess) => {
+	for(let i = 0 ; i<correctGuess.length;i++){
+		if(correctGuess[i]==guess){
+			return true;
+		}
+	}
+	return false
+}
+
+const checkIfWinner = () => {
+	if(correctGuess.length>=5){
+		$('.verdict').html("You Won! Congrats!");
+
+	}
+}
